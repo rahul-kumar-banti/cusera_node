a07a50826a187bccfce502fc06a665fb2edf032b
rahulbanti36 <rahulbanti36@gmail.com>
2019-01-09 16:29:22 +0530

    express set static path

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express=require("express");
 const http=require("http");
+const morgan=require("morgan");
 const host="localhost";
 const port=5555;
 const app=express();
-
+app.use(morgan("dev"));
+app.use(express.static(__dirname+"/public"));
 app.use((req,res,next)=>{
-    console.log(req.url);
     res.setHeader("Content-Type","text/html");
     res.statusCode=200;
     res.end("<h3 style='color:red'>welcome to express</h3>");
